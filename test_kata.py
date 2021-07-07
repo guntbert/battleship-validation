e2e52abb664c3a6bc084393a13b496bcd4b88cd0
G. Reiter <guntbert@gmx.at>
2021-07-07 21:54:13 +0200

    WIP: Prepare refactoring of detect-...

--- test_kata.py
+++ test_kata.py
@@ -1,4 +1,6 @@
 import kata
+import pytest
+
 # This battlefield is not a valid one, I use it to test the detecting functions
 battleField =   [[1, 0, 0, 0, 0, 1, 1, 0, 0, 0],
                  [1, 0, 1, 0, 0, 0, 0, 0, 1, 0],
@@ -30,9 +32,18 @@ def test_detect_horizontal_threes():
 def test_detect_vertical_threes():
     assert kata.detect_vert_threes(battleField) == 1
 
-def test_detect_horizontal_fours():
-    assert kata.detect_hor_fours(battleField) == 1
+@pytest.mark.parametrize("size, expected_count", [
+    (2,1),
+    (3,1),
+    (4,1)
+])
+def test_count_horizontal_ships(size, expected_count):
+    assert kata.count_hor_ships(battleField, size) == expected_count
 
-def test_detect_vertical_fours():
-    assert kata.detect_vert_fours(battleField) == 1
+
+# def test_detect_horizontal_fours():
+#     assert kata.detect_hor_fours(battleField) == 1
+
+# def test_detect_vertical_fours():
+#     assert kata.detect_vert_fours(battleField) == 1
 
