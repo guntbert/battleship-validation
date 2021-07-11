2e0ea1754222430bad41cdf7106424e01eae334d
G. Reiter <guntbert@gmx.at>
2021-07-11 23:06:26 +0200

    Apply formatting suggestions

--- kata.py
+++ kata.py
@@ -1,29 +1,31 @@
 ships = {}
 
+
 def count_ones(field):
     # '1' and only '0' (or edge) in all 8 positions around
     count = 0
-    for y,row in enumerate(field):
-        for x,item in enumerate(row):
-            if item == 1 and neighbors_are_0_or_edge(field,[(y,x)]):
+    for y, row in enumerate(field):
+        for x, item in enumerate(row):
+            if item == 1 and neighbors_are_0_or_edge(field, [(y, x)]):
                 count += 1
     return count
+
 
 def count_hor_ships(field, size):
     ''' size > 1 (for one-ships we still will use count_ones()) '''
     # '1' followed by size-1 '1' to the right and then a '0'
     count = 0
-    for y,row in enumerate(field):
-        for x,item in enumerate(row[:-size]):
+    for y, row in enumerate(field):
+        for x, item in enumerate(row[:-size]):
             if item == 1:
-                    # breakpoint()
-                check_positions = [(y, xx) for xx in range(x,x+size) ]
-                check_values = [field[y][xx] for xx in range(x,x+size)]
+                # breakpoint()
+                check_positions = [(y, xx) for xx in range(x, x + size)]
+                check_values = [field[y][xx] for xx in range(x, x + size)]
                 if size > 2:
                     print(f"{check_positions=}  ~ {check_values=}")
                 if 0 not in check_values:
-                    if field[y][x+size] == 0 :
-                        if neighbors_are_0_or_edge(field,check_positions):
+                    if field[y][x + size] == 0:
+                        if neighbors_are_0_or_edge(field, check_positions):
                             print(f'before inc: {count}')
                             count += 1
                             print(f'after inc: {count}')
@@ -33,51 +35,55 @@ def count_hor_ships(field, size):
 def detect_hor_threes(field):
     # '1' accompanied by two '1' to the right and then a '0'
     count = 0
-    for y,row in enumerate(field):
-        for x,item in enumerate(row[:-3]):
+    for y, row in enumerate(field):
+        for x, item in enumerate(row[:-3]):
             # print(f"+ {y=}/{x=} {item=} {field[y][x+1]=}  {field[y][x+2]=} {field[y][x+3]=}")
             if item == 1 \
-                and field[y][x+1] == 1 and field[y][x+2] ==1 and field[y][x+3] == 0 \
-                and neighbors_are_0_or_edge(field,[(y,x),(y,x+1), (y, x+2)]):
+                    and field[y][x + 1] == 1 and field[y][x + 2] == 1 and field[y][x + 3] == 0 \
+                    and neighbors_are_0_or_edge(field, [(y, x), (y, x + 1), (y, x + 2)]):
                 # print(f"+ {y=}/{x=}")
                 count += 1
     return count
+
 
 def detect_vert_threes(field):
     # '1' accompanied by a '1' below and then a '0'
     count = 0
-    for y,row in enumerate(field[:-3]):
-        for x,item in enumerate(row):
+    for y, row in enumerate(field[:-3]):
+        for x, item in enumerate(row):
             if item == 1 \
-                and field[y+1][x] == 1 and field[y+2][x] == 1 and field[y+3][x] == 0\
-                and neighbors_are_0_or_edge(field,[(y,x),(y+1,x), (y+2,x)]):
-                # print(f"+ {y=}/{x=}")
-                count += 1
-    return count
-    
-def detect_hor_twos(field):
-    # '1' accompanied by a '1' to the right and then a '0'
-    count = 0
-    for y,row in enumerate(field):
-        for x,item in enumerate(row[:-2]):
-            if item == 1 \
-                and field[y][x+1] == 1 and field[y][x+2] ==0  \
-                and neighbors_are_0_or_edge(field,[(y,x),(y,x+1)]):
+                    and field[y + 1][x] == 1 and field[y + 2][x] == 1 and field[y + 3][x] == 0 \
+                    and neighbors_are_0_or_edge(field, [(y, x), (y + 1, x), (y + 2, x)]):
                 # print(f"+ {y=}/{x=}")
                 count += 1
     return count
 
+
+def detect_hor_twos(field):
+    # '1' accompanied by a '1' to the right and then a '0'
+    count = 0
+    for y, row in enumerate(field):
+        for x, item in enumerate(row[:-2]):
+            if item == 1 \
+                    and field[y][x + 1] == 1 and field[y][x + 2] == 0 \
+                    and neighbors_are_0_or_edge(field, [(y, x), (y, x + 1)]):
+                # print(f"+ {y=}/{x=}")
+                count += 1
+    return count
+
+
 def detect_vert_twos(field):
     # '1' accompanied by a '1' below and then a '0'
     count = 0
-    for y,row in enumerate(field[:-2]):
-        for x,item in enumerate(row):
+    for y, row in enumerate(field[:-2]):
+        for x, item in enumerate(row):
             if item == 1 \
-                and field[y+1][x] == 1 and field[y+2][x] == 0 \
-                and neighbors_are_0_or_edge(field,[(y,x),(y+1,x)]):
+                    and field[y + 1][x] == 1 and field[y + 2][x] == 0 \
+                    and neighbors_are_0_or_edge(field, [(y, x), (y + 1, x)]):
                 count += 1
     return count
-    
+
+
 def get_orientation(poslist):
     ''' 
     determine the orientation by checking the first two items
@@ -90,65 +96,67 @@ def get_orientation(poslist):
         return 'vert'
     else:
         return 'hor'
-    
+
+
 def neighbors_are_0_or_edge(field, poslist):
     orientation = get_orientation(poslist)
     # print(f"{poslist =};{orientation =}")
     if orientation == 'hor':
         # breakpoint()
         # check 3 values on the left, 3 values on the right, above/below for all
-        for index,pos in enumerate(poslist):
+        for index, pos in enumerate(poslist):
             # if len(poslist) >1:
-                # print(f"{index=} {pos=}")
-            if index == 0 and pos[1] > 0:   # we only need to check on the left side if we are no at the edge anyway
-                for y in range(max(pos[0]-1,0), min(pos[0]+1,len(field)-1)+1):
-                    x = max(pos[1]-1,0) # TODO rhs should be simplified to just pos[1]
-                    print("hor left",y,x)
-                    if field[y][x]  != 0:
+            # print(f"{index=} {pos=}")
+            if index == 0 and pos[1] > 0:  # we only need to check on the left side if we are no at the edge anyway
+                for y in range(max(pos[0] - 1, 0), min(pos[0] + 1, len(field) - 1) + 1):
+                    x = max(pos[1] - 1, 0)  # TODO rhs should be simplified to just pos[1]
+                    print("hor left", y, x)
+                    if field[y][x] != 0:
                         # print("XX")
                         return False
-            if index == len(poslist)-1 and pos[1] < len(field) - 1:
-                for y in range(max(pos[0]-1,0), min(pos[0]+1,len(field)-1)+1):
-                    x = min(pos[1]+1,len(field)-1)  # TODO same as before
-                    print("hor right",y,x)
-                    if field[y][x]  != 0:
+            if index == len(poslist) - 1 and pos[1] < len(field) - 1:
+                for y in range(max(pos[0] - 1, 0), min(pos[0] + 1, len(field) - 1) + 1):
+                    x = min(pos[1] + 1, len(field) - 1)  # TODO same as before
+                    print("hor right", y, x)
+                    if field[y][x] != 0:
                         # print("XX")
                         return False
             x = pos[1]
-            for y in (max(pos[0]-1,0),min(pos[0]+1,len(field)-1)):
-                print("hor inner",y,x)
+            for y in (max(pos[0] - 1, 0), min(pos[0] + 1, len(field) - 1)):
+                print("hor inner", y, x)
                 if field[y][x] != 0 and y != pos[0]:
                     # print("XX")
                     return False
     else:
         # check 3 values above, 3 values below, right/left for all
-        for index,pos in enumerate(poslist):
+        for index, pos in enumerate(poslist):
             # print(f"{index=} {pos=}")
             if index == 0:
-                for x in range(max(pos[1]-1,0), min(pos[1]+1,len(field)-1)+1):
-                    y = max(pos[0]-1,0)
+                for x in range(max(pos[1] - 1, 0), min(pos[1] + 1, len(field) - 1) + 1):
+                    y = max(pos[0] - 1, 0)
                     # print("vert above",y,x)
-                    if field[y][x]  != 0:
+                    if field[y][x] != 0:
                         return False
-            if index == len(poslist)-1:
-                for x in range(max(pos[1]-1,0), min(pos[1]+1,len(field)-1)+1):
-                    y = min(pos[0]+1,len(field)-1)
+            if index == len(poslist) - 1:
+                for x in range(max(pos[1] - 1, 0), min(pos[1] + 1, len(field) - 1) + 1):
+                    y = min(pos[0] + 1, len(field) - 1)
                     # print("vert below",y,x)
-                    if field[y][x]  != 0:
+                    if field[y][x] != 0:
                         return False
             y = pos[0]
-            for x in (max(pos[1]-1,0),min(pos[1]+1,len(field)-1)):
+            for x in (max(pos[1] - 1, 0), min(pos[1] + 1, len(field) - 1)):
                 # print("vert inner",y,x)
                 if field[y][x] != 0 and x != pos[1]:
                     return False
     return True
 
+
 def validate_battlefield(field):
-# 1 * 4, 2 * 3, 3 * 2, 4 * 1 
-# Field: 10x10
-# Rules
-# - ships must be straight (no corners)
-# - they may not touch/overlap
+    # 1 * 4, 2 * 3, 3 * 2, 4 * 1
+    # Field: 10x10
+    # Rules
+    # - ships must be straight (no corners)
+    # - they may not touch/overlap
     # print(f"1-ships: {count_ones(field)}")
     # print(f"horizontal 2-ships: {detect_hor_twos(field)}")
     # print(f"vertical 2-ships: {detect_vert_twos(field)}")
--- test_kata.py
+++ test_kata.py
@@ -1,17 +1,20 @@
-import kata
 import pytest
 
+import kata
+
 # This battlefield is not a valid one, I use it to test the detecting functions
-battleField =   [[1, 0, 0, 0, 0, 1, 1, 0, 0, 0],
-                 [1, 0, 1, 0, 0, 0, 0, 0, 1, 0],
-                 [1, 0, 1, 0, 1, 1, 1, 0, 1, 0],
-                 [1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                 [0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
-                 [0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
-                 [0, 0, 0, 0, 0, 1, 0, 0, 1, 0],
-                 [0, 0, 0, 1, 0, 1, 0, 0, 0, 0],
-                 [0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
-                 [1, 1, 1, 1, 0, 0, 0, 0, 0, 0]]
+battleField = [[1, 0, 0, 0, 0, 1, 1, 0, 0, 0],
+               [1, 0, 1, 0, 0, 0, 0, 0, 1, 0],
+               [1, 0, 1, 0, 1, 1, 1, 0, 1, 0],
+               [1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+               [0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
+               [0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
+               [0, 0, 0, 0, 0, 1, 0, 0, 1, 0],
+               [0, 0, 0, 1, 0, 1, 0, 0, 0, 0],
+               [0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
+               [1, 1, 1, 1, 0, 0, 0, 0, 0, 0]]
+
+
 # test.assert_equals(validate_battlefield(battleField), True, "Yep! Seems alright", "Nope, something is wrong!");
 
 def test_count_ones():
@@ -29,21 +32,21 @@ def test_detect_vertical_twos():
 def test_detect_horizontal_threes():
     assert kata.detect_hor_threes(battleField) == 1
 
+
 def test_detect_vertical_threes():
     assert kata.detect_vert_threes(battleField) == 1
 
+
 @pytest.mark.parametrize("size, expected_count", [
-    (2,1),
-    (3,1),
-    (4,1)
+    (2, 1),
+    (3, 1),
+    (4, 1)
 ])
 def test_count_horizontal_ships(size, expected_count):
     assert kata.count_hor_ships(battleField, size) == expected_count
-
 
 # def test_detect_horizontal_fours():
 #     assert kata.detect_hor_fours(battleField) == 1
 
 # def test_detect_vertical_fours():
 #     assert kata.detect_vert_fours(battleField) == 1
-
